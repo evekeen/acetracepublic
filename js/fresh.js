e53d0d627383e0b05f04a54bd080ed30df6d4546
Alexander Ivkin <a@ivkin.dev>
2023-10-31 10:47:03 +0100

    SEO optimization

--- js/fresh.js
+++ js/fresh.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
     
     //Preloader
     $(window).on('load', function() { // makes sure the whole site is loaded 
-        $('#status').fadeOut(); // will first fade out the loading animation 
-        $('#preloader').fadeOut('slow'); // will fade out the white DIV that covers the website.
+        // $('#status').fadeOut(); // will first fade out the loading animation
+        // $('#preloader').fadeOut('slow'); // will fade out the white DIV that covers the website.
         $('body').css({'overflow':'visible'});
     })
 
